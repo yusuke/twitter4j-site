a590a497854ee327c2fc76169c715462459ae1ed
Yusuke Yamamoto <yusuke@mac.com>
2009-11-25 01:16:32 +0900

    latest test cases

--- testcases/get-testcases.sh
+++ testcases/get-testcases.sh
@@ -15,18 +15,18 @@ function downloadSearchAs(){
 mkdir -p $1
 wget --user=twit4j --password=twitter4j http://search.twitter.com/$1$2 -O $1$3
 }
-download "" search.json?q=twitter
-download "" trends.json
+#download "" search.json?q=twitter
+#download "" trends.json
 
-downloadSearch trends/ current.json
-downloadSearch trends/ daily.json
-downloadSearch trends/ weekly.json
-download statuses/ public_timeline.json
-download statuses/ home_timeline.json
-download statuses/ show/123.xml
-postdownload statuses/retweet/ 2245071380.json ""
-download users/show/ twit4j.json
-download statuses/followers/ T4J_hudson.json
-download statuses/friends/ T4J_hudson.json
-download statuses/user_timeline/ testiverse.json 
-downloadSearchAs trends/ daily.json?date=2004-03-19 daily-empty.json
+#downloadSearch trends/ current.json
+#downloadSearch trends/ daily.json
+#downloadSearch trends/ weekly.json
+#download statuses/ public_timeline.json
+#download statuses/ home_timeline.json
+#download statuses/ show/123.xml
+postdownload statuses/retweet/ 6010814202.json ""
+#download users/show/ twit4j.json
+#download statuses/followers/ T4J_hudson.json
+#download statuses/friends/ T4J_hudson.json
+#download statuses/user_timeline/ testiverse.json 
+#downloadSearchAs trends/ daily.json?date=2004-03-19 daily-empty.json
