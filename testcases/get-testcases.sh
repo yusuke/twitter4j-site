67392077b785a9f273c1af177ce7c0a67d82c934
Yusuke Yamamoto <yusuke@mac.com>
2009-11-25 02:34:01 +0900

    test cases for geo location api

--- testcases/get-testcases.sh
+++ testcases/get-testcases.sh
@@ -24,8 +24,9 @@ wget --user=twit4j --password=twitter4j http://search.twitter.com/$1$2 -O $1$3
 #download statuses/ public_timeline.json
 #download statuses/ home_timeline.json
 #download statuses/ show/123.xml
-postdownload statuses/retweet/ 6010814202.json ""
-#download users/show/ twit4j.json
+#postdownload statuses/retweet/ 6010814202.json ""
+download users/show/ twit4j.json
+download users/show/ twit4j2.json
 #download statuses/followers/ T4J_hudson.json
 #download statuses/friends/ T4J_hudson.json
 #download statuses/user_timeline/ testiverse.json 
